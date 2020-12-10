9305a39b2a8af577cc471bad990c3f1ee9a635ad
ksdhk <47198182+ksdhk@users.noreply.github.com>
2020-12-10 23:41:20 +0900

    duc_test
    test

--- src/Duck.java
+++ src/Duck.java
@@ -1,7 +1,10 @@
 public class Duck {
-
+    int size;
     public Duck(){
         System.out.println("Quack");
+    }
+    public void setSize(int newSize){
+        size = newSize;
     }
 }
 
--- src/UseADuck.java
+++ src/UseADuck.java
@@ -1,5 +1,7 @@
 public class UseADuck {
     public static void main(String[] args) {
+
         Duck d = new Duck();
+        d.setSize(42);
     }
 }
