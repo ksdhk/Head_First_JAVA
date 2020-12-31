2d5a8546d3906b8b6cab963cbcc82a9e49eeee75
ksdhk <47198182+ksdhk@users.noreply.github.com>
2021-01-01 01:23:19 +0900

    test
    test

--- src/TestHippo.java
+++ src/TestHippo.java
@@ -2,5 +2,6 @@ public class TestHippo {
     public static void main(String[] args) {
         System.out.println("Starting...");
         Hippo H = new Hippo();
+        System.out.println("Test");
     }
 }
