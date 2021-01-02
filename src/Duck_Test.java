70b2cd3850843e88a33b51e7a925e16e877ff978
ksdhk <47198182+ksdhk@users.noreply.github.com>
2021-01-03 01:45:50 +0900

    Create Duck_Test.java
    test

---
+++ src/Duck_Test.java
@@ -0,0 +1,16 @@
+public class Duck_Test {
+
+private int size;
+private static int duckCount = 0;
+
+public Duck_Test(){
+    duckCount++;
+}
+
+public void setSize(int s){
+    size = s;
+}
+public int getSize(){
+    return size;
+}
+}
