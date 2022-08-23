5dd896afa3ecc9c0be9d50cceca758a6d9a8e6b7
ksdhk <ksdhk@naver.com>
2022-08-23 18:46:33 +0900

    Hello World

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    /**
+     * Date : 2022.08.23
+     * Source : Hello.java
+     * Author : 신동훈
+     * Email : ksdhk@naver.com
+     * */
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
